a3fe31ae15d7f3985c22123142c510f6698d0527
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-10 23:05:37 +0000

    fixes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 for i in range(1, 100):
-	if (i / 10) == (i % 10):
-		continue
-	elif (i / 10) > (i % 10):
-		continue
-	else:
-		print('{:02}'.format(i), end="")
-		if i < 89:
-			print(',', end=" ")
+    if (i / 10) == (i % 10):
+        continue
+    elif (i / 10) > (i % 10):
+        continue
+    else:
+        print('{:02}'.format(i), end="")
+    if i < 89:
+            print(',', end=" ")
 print('')
