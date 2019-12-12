0c7a2abd633fb266222b961c4ad5393c10964c07
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-12 19:20:41 +0000

    fix hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    for i in range(0, len(dir(hidden_4))):
-        if name[0:2] != '__':
-            print('{}'.format(dir(hidden)[i]))
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0:2] != '__':
+            print(dir(hidden_4)[i])
