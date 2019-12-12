cb17221f4315268b0d0a0cf5b569dd2037fa1f46
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-12 19:01:14 +0000

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    for i in range(0, len(dir(hidden_4))):
+        if name[0:2] != '__':
+            print('{}'.format(dir(hidden)[i]))
