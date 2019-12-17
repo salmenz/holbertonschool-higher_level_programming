34b34f127783b855c7dc9200679c4084f71d2006
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-17 23:03:10 +0000

    add type condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new = my_list.copy()
-    for i in reversed(new):
-        print(i)
+    if type(my_list) is list:
+        for i in reversed(new):
+            print(i)
