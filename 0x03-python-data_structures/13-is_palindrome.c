3b23118cefe5167670ef11537ae80440860dd962
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-18 10:05:44 +0000

    fix in case of empty

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,37 @@
-d
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * is_palindrome - Tests if thr=e list is a palindrome
+ * @head: The head
+ *
+ * Return: 1 if palindrome and 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+listint_t *f = *head;
+listint_t *d = *head;
+int l = 0, i = 0, j = 0;
+
+if (*head == NULL)
+return (1);
+while (f->next != NULL)
+{
+f = f->next;
+l++;
+}
+if (d->n == f->n)
+{
+while (i <= (l / 2))
+{
+f = *head;
+for (j = 0; j < (l - i); j++)
+f = f->next;
+if (d->n != f->n)
+return (0);
+i++;
+d = d->next;
+}
+}
+return (1);
+}
