1bb81fabbfabdceba115529a23fdc44511ed6137
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-21 20:34:00 +0000

    covert from roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) is not str:
+    if roman_string is not None or type(roman_string) is not str:
         return(0)
     else:
         sum = 0
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
         for i in range(len(roman_string)):
             for j in range(len(tuple1)):
                 if roman_string[i] == tuple1[j]:
-                    if tuple2[j] <= tuple2[j+1] and tuple2[j+1] != None:
+                    if tuple2[j] <= tuple2[j+1] and tuple2[j+1] is not None:
                         sum += tuple2[j]
                     else:
                         sum -= tuple2[j]
