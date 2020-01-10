856f31bf0ee1dd847d9eade8cd24bb8b1217e4c1
salmenz <salmen.zooro@gmail.com>
2020-01-10 13:32:31 +0100

    add exception valueerror

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-    except Error:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
         print("{} is not an integer".format(value))
         return False
+    else:
+        return True
