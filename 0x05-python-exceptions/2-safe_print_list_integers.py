2ac75c480940503c4a61598dd1b721dcef589591
salmenz <salmen.zooro@gmail.com>
2020-01-10 14:03:48 +0100

    fix the pos of len

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     len = 0
     for i in range(x):
-        len += 1
         try:
             print("{:d}".format(my_list[i]), end="")
+            len += 1
         except (ValueError, TypeError):
             pass
     print()
