0a841cf8206119a7976011c27c738c4f72a30c5a
salmenz <salmen.zooro@gmail.com>
2020-01-10 21:46:45 +0000

    fix pep8 for 100 and 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_function(fct, *args):
     try:
         f = fct(*args)
         return (f)
     except Exception as error:
+        import sys
         print("Exception: {}".format(error), file=sys.stderr)
         return None
