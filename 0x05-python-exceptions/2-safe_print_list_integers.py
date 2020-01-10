17de3505dedfa0bf73a25de01161a5c21cd35125
salmenz <salmen.zooro@gmail.com>
2020-01-10 13:58:27 +0100

    add pass to except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         len += 1
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
             pass
     print()
