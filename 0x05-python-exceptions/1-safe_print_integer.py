00e4929490f9277973734a30e57e4e45da0b3c1d
salmenz <salmen.zooro@gmail.com>
2020-01-10 13:25:38 +0100

    safe print 0 and 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except Error:
+        print("{} is not an integer".format(value))
+        return False
