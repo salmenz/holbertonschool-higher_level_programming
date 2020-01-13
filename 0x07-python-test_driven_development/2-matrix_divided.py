dd00feee84147bc25d64db230673fd90587a7389
salmenz <salmen.zooro@gmail.com>
2020-01-13 16:23:16 +0000

    fix matrix and task 3 and 4

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    new_matrix = matrix.copy()
-    l = len(new_matrix[0])
-    for i in range (len(new_matrix)):
-        if type(new_matrix[i]) != list:
+    new_matrix = []
+    m = []
+    l = len(matrix[0])
+    for i in range (len(matrix)):
+        if type(matrix[i]) != list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(new_matrix[i]) != l:
+        if len(matrix[i]) != l:
             raise TypeError("Each row of the matrix must have the same size")
-        for j in range(len(new_matrix[i])):
-            if type(new_matrix[i][j]) != int and type(new_matrix[i][j]) != float:
+        m = []
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             if div == 0:
                 raise ZeroDivisionError("division by zero")
-            new_matrix[i][j] = round(new_matrix[i][j] / div, 2)
+            m.append(round(matrix[i][j] / div, 2))
+        new_matrix.append(m)
     return new_matrix
