5f813eef51b4288a597371624105be9d994bd616
salmenz <salmen.zooro@gmail.com>
2020-01-17 11:25:02 +0100

    add Change representationand Compare rectangles and Compare rectangles

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+class Rectangle:
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        str = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                str += "{}".format(self.print_symbol)
+            str += '\n'
+        return str
+
+    def __repr__(self):
+        return 'Rectangle({}, {})'.format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        del(self)
+        Rectangle.number_of_instances -= 1
+
