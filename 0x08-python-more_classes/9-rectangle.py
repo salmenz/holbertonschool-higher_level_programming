a8966b927cabe063f8931b8e27499504469300fe
salmenz <salmen.zooro@gmail.com>
2020-01-18 22:21:30 +0000

    fix str method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,8 +4,20 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if type(width) == int:
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = width
+        else:
+            raise TypeError("width must be an integer")
+        if type(height) == int:
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = height
+        else:
+            raise TypeError("height must be an integer")
         Rectangle.number_of_instances += 1
 
     @property
