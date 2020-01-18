c28735e3bfeb235f7fd39225866e3b6132f7c81b
salmenz <salmen.zooro@gmail.com>
2020-01-18 21:34:09 +0000

    fix exeption

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if type(width) == int:
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = width
+        else:
+            raise TypeError("width must be an integer")
+        if type(height) == int:
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = height
+        else:
+            raise TypeError("height must be an integer")
 
     @property
     def width(self):
