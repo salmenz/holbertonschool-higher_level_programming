bb59b432f396669619c0e2ce0b96085648293609
salmenz <salmen.zooro@gmail.com>
2020-01-20 20:18:21 +0000

    fix indentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,4 +5,4 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) != str:
         raise TypeError("last_name must be a string")
     else:
-        print ("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
