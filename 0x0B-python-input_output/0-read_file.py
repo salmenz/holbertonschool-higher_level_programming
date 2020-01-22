2a13a5d3e2947c3f1e18cd1cc24373b50e302013
salmenz <salmen.zooro@gmail.com>
2020-01-22 22:02:19 +0000

    add .format to print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """ Function that read a text file (UTF8) and prints it stdout """
     with open(filename, encoding="UTF-8") as File:
-        print ("{:s}".format(File.read()), end="")
+        print("{}".format(File.read()), end="")
