eec2d843f5ac5f226e8d3ce3e8a3c76cea7b2109
salmenz <salmen.zooro@gmail.com>
2020-01-22 21:52:23 +0000

    add .format to print

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,11 +3,10 @@ def read_lines(filename="", nb_lines=0):
     number_of_lines = __import__('1-number_of_lines').number_of_lines
     if number_of_lines(filename) <= nb_lines or nb_lines <= 0:
         with open(filename, encoding="UTF-8") as File:
-            print(File.read())
+            print("{}".format(File.read()), end="")
     else:
         line = 1
         with open(filename, encoding="UTF-8") as File:
             while nb_lines >= line:
-                print(File.readline())
+                print("{}".format(File.readline()), end="")
                 line += 1
-            
