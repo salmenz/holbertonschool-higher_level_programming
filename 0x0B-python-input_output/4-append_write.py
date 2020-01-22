39179dc908d2c6aa810b86b7b9e9f244e8ca9fd0
salmenz <salmen.zooro@gmail.com>
2020-01-22 22:30:49 +0000

    add appending into a file

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     with open(filename, "a", encoding="UTF8") as File:
         return File.write(text)
