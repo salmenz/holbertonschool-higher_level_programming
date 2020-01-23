e9b7dab69acad55fd3c23faa53f1d7d061d1d7e5
salmenz <salmen.zooro@gmail.com>
2020-01-23 20:38:29 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            for item in attrs:
+                if type(item) != str:
+                    return self.__dict__
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
