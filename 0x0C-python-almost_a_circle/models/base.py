2932026cb409b83003eeda5020825062244cfd25
salmenz <salmen.zooro@gmail.com>
2020-01-31 18:47:21 +0000

    Load from file json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,33 @@ class Base:
                     i = i.to_dictionary()
                     list.append(i)
                 File.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns json_string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance of class"""
+        New = cls(69, 1, 2)
+        New.update(**dictionary)
+        return New
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Return a list of instances from the json file
+        """
+        try:
+            with open(cls.__name__+'.json', encoding='utf-8') as File:
+                data = File.read()
+                object = cls.from_json_string(data)
+                list = []
+                for i in object:
+                    list.append(cls.create(**i))
+                return list
+        except FileNotFoundError:
+            return []
