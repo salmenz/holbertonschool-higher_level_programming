30802998d3e6acb102458d5a723ea52258042444
salmenz <salmen.zooro@gmail.com>
2020-03-03 18:32:48 +0000

    SQL command

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,1 +1,2 @@
+-- script that deletes the database hbtn_0c_0 in your MySQL server.
 drop database if exists hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,1 +1,2 @@
+-- script that creates a table called first_table in the current database in your MySQL server.
 create table first_table (id int, name varchar(256));
