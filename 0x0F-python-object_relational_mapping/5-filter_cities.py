6a7cff0d787bf9b06513a7ddcfddd7ccdc00ee10
salmenz <salmen.zooro@gmail.com>
2020-03-29 20:21:33 +0000

    list all the cities of a state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN ON states\
+    cur.execute("SELECT cities.name FROM cities JOIN states ON\
                  cities.state_id = states.id WHERE states.name \
                  LIKE '{}' ORDER BY cities.id".format(sys.argv[4]))
     query_rows = cur.fetchall()
