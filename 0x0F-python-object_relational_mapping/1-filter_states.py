84b9f29f8400a744a4f39b2b8fe975be940b4c97
salmenz <salmen.zooro@gmail.com>
2020-03-29 09:08:43 +0000

    states start with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE "N*" ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name = "N*" ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
