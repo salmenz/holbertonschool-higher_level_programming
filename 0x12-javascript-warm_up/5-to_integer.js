f9cee509382259925636e7090b36910eb758768f
salmenz <salmen.zooro@gmail.com>
2020-05-05 21:53:28 +0000

    js tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (process.argv[2] && process.argv[2] !== null) {
   console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
