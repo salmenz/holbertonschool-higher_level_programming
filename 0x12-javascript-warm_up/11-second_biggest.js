44b186f62c87f1777f2709ea573a0b3ab666bffb
salmenz <salmen.zooro@gmail.com>
2020-05-06 17:07:40 +0000

    fix sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,5 +6,5 @@ if (process.argv.length < 4) {
   for (let i = 2; i < process.argv.length; i++) {
     t.push(process.argv[i]);
   }
-  console.log(t.sort().reverse()[1]);
+  console.log(t.sort((a, b) => a - b).reverse()[1]);
 }
