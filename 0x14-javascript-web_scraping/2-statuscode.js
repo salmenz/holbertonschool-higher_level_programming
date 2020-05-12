7738a3e2d4a67a272c21dcd30962e34d5f00ec67
salmenz <salmen.zooro@gmail.com>
2020-05-12 22:52:04 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,8 @@
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
   if (err) {
-    console.log(err);}
-  console.log('code:', response.statusCode);
+    console.log(err);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
