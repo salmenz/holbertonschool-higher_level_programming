7e4fdb871740668d6ceed3b9a51ea461dfc96590
salmenz <salmen.zooro@gmail.com>
2020-05-13 22:23:21 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
-const url = 'http://swapi.co/api/films/' + process.argv[2] + '/';
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
     console.log(JSON.parse(body).title);
   }
